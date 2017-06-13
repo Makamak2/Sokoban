0a9598952ac07058c3be115f6406f6074bb78d5b
Marek Kocvara <kocvara.mark@gmail.com>
2017-06-13 14:03:02 +0200

    Player can now push boxes and the level is completed when all boxes are on a goal.

--- sokoban.c
+++ sokoban.c
@@ -1,7 +1,8 @@
-// Inclusion of used libraries
+// Inclusion of used libraries and headers
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h> // For the usage of boolean data type.
+#include "stack.h"
 // End of inclusion
 
 // Changeable definition of the height and the width of the board ( is used as parameters when declaring the array )
@@ -23,6 +24,16 @@ char array[WIDTH][HEIGHT][2];
 int col_player;
 int row_player;
 // The end of the declaration.
+
+// Defining functions from header "stack.h"
+void init( element** a );
+int is_empty( element* a );
+void push( element** a, int b );
+int top( element* a );
+void pop( element** a );
+void remove_stack( element** a );
+// End of definitions
+
 
 // The start of function 'prepare_level()', which sets the elements in the array to walls, goals, boxes and the player.
 void prepare_level1()
@@ -81,9 +92,9 @@ void prepare_level1()
 	array[10][3][0] = GOAL;	
 	array[10][8][0] = GOAL;
 
-	array[7][4][0] = BOX;
-	array[8][5][0] = BOX;
-	array[9][4][0] = BOX;
+	array[7][4][1] = BOX;
+	array[8][5][1] = BOX;
+	array[9][4][1] = BOX;
 }
 // The end of function 'prepare_level'.
 
@@ -108,26 +119,112 @@ void print_board()
     printf("\n");
    }
 	printf("\ncol_player: %d, row_player: %d\n", col_player, row_player);
-	printf("\nChar on [6,8]: %c\n", array[6][8][0]);
+	printf("\nChar on [7,4]: %c\n", array[7][4][1]);
 }
 // The end of function 'print_board()'.
 
+// The start of function 'check_goals()', which checks if all boxes are on goals.
+void check_goals()
+{
+	int goals = 3;
+	int on_goal = 0;
+
+	for( int row = 0; row < WIDTH; row++ )
+	{
+   	for( int col = 0; col < HEIGHT; col++ )
+      {
+			if( array[col][row][0] == GOAL && array[col][row][1] == BOX )
+			{
+				on_goal++;
+			}
+			if( goals == on_goal )
+			{	
+				system( "printf \"\033c\"" );
+				printf("Well played!\n\n");
+				exit( 0 );
+			}
+		}
+	}
+}
 //
+
+// The start of function 'box_movement()', which checks if player can move a box and moves it if he can.
+bool box_movement( int coordinate_x, int coordinate_y )
+{
+	if( coordinate_x == col_player && coordinate_y == row_player+1 ) // Player is above the box
+	{
+		if( array[coordinate_x][coordinate_y+1][1] == BOX || array[coordinate_x][coordinate_y+1][0] == WALL )
+		{
+			return false;
+		}
+		array[coordinate_x][coordinate_y+1][1] = BOX;
+		array[coordinate_x][coordinate_y][1] = '0';
+		return true;
+	}
+
+	if( coordinate_x == col_player && coordinate_y == row_player-1 ) // Player is beneath the box
+	{
+		if( array[coordinate_x][coordinate_y-1][1] == BOX || array[coordinate_x][coordinate_y-1][0] == WALL )
+		{
+			return false;
+		}
+		array[coordinate_x][coordinate_y-1][1] = BOX;
+		array[coordinate_x][coordinate_y][1] = '0';
+		return true;
+	}
+
+	if( coordinate_x == col_player+1 && coordinate_y == row_player )	// Player is to the left of the box
+	{
+		if( array[coordinate_x+1][coordinate_y][1] == BOX || array[coordinate_x+1][coordinate_y][0] == WALL )
+		{
+			return false;
+		}
+		array[coordinate_x+1][coordinate_y][1] = BOX;
+		array[coordinate_x][coordinate_y][1] = '0';
+		return true;
+	}
+
+	if( coordinate_x == col_player-1 && coordinate_y == row_player )	// Player is to the right of the box
+	{
+		if( array[coordinate_x-1][coordinate_y][1] == BOX || array[coordinate_x-1][coordinate_y][0] == WALL )
+		{
+			return false;
+		}
+		array[coordinate_x-1][coordinate_y][1] = BOX;
+		array[coordinate_x][coordinate_y][1] = '0';
+		return true;
+	}
+	
+	system( "printf \"\033c\"" );
+	printf("\nERROR\n");
+	exit( 1 );
+
+}
+// End of function 'box_movement'.
+
+// The start of function 'check_player_step(), which checks the tile where the player wants to move.
 bool check_player_step( int coordinate_x, int coordinate_y )
 {
 	if( array[coordinate_x][coordinate_y][0] == WALL  || coordinate_x > ( WIDTH-1 ) || coordinate_x < 0 || coordinate_y > ( HEIGHT-1 ) || coordinate_y < 0 )
 	{
 		return false;
 	}
+
+	if( array[coordinate_x][coordinate_y][1] == BOX )
+	{
+		return box_movement( coordinate_x, coordinate_y );
+	}
+
 	return true;
 }
-// The end of 'check
+// The end of function 'check_player_step()'
 
 // The start of function 'move_player()', which moves the player according to the input from main().
 bool move_player( int coordinate_x, int coordinate_y )
 {
 	if( check_player_step( coordinate_x, coordinate_y ) == false)
 	{
+		print_board();
 		return false;
 	}
 	col_player = coordinate_x;
@@ -145,6 +242,8 @@ int main()
 
 	while( 1 )
 	{
+		check_goals();
+
 		system("/bin/stty raw");
    	char direction = getchar();
    	system("/bin/stty cooked");
